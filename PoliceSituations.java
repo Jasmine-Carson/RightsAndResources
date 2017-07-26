c83786f4e0cf0e05f0e77eb3f5b9b0cc54b1d9b0
Jazzy <fairy7823@gmail.com>
2017-07-26 13:24:08 -0700

    final pages

---
+++ PoliceSituations.java
@@ -0,0 +1,13 @@
+package com.example.girlswhocode.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PoliceSituations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_police_situations);
+    }
+}
